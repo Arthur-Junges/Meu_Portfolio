8f34f8f9a4448192cff9e10bcfb96ae62c510b6b
Arthur-Junges <Arthur.jungesH@gmail.com>
2026-07-11 13:51:12 -0300

    animação do mouse

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
-const cursor = document.getElementById('#cursor')
+const glow = document.getElementById('cursor')
 
-document.addEventListener("mousemove" ,(e) => {
-    cursor.style.left = e.clientX + "px";
-    cursor.style.top = e.clientY + "px";
+let mouseX = 0, mouseY = 0
+let glowX = 0, glowY = 0
+
+window.addEventListener('mousemove', (e) => {
+    mouseX = e.clientX
+    mouseY = e.clientY
 })
+
+function animate() {
+    glowX += (mouseX - glowX) * 0.08
+    glowY += (mouseY - glowY) * 0.08
+
+    glow.style.left = glowX + 'px'
+    glow.style.top = glowY + 'px'
+
+    requestAnimationFrame(animate)
+}
+
+animate()
