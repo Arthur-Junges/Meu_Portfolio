c0fcba99e47428e3729701a741f27ccb46413e87
Arthur-Junges <Arthur.jungesH@gmail.com>
2026-07-11 15:50:44 -0300

    barra de progresso e niveis automatizado com js

--- script.js
+++ script.js
@@ -19,3 +19,37 @@ function animate() {
 }
 
 animate()
+
+const barras = document.querySelectorAll(".progresso")
+
+window.addEventListener("load", () => {
+    barras.forEach(barra => {
+        const porcentagem = Number(barra.dataset.progresso)
+        const nivel = barra.closest(".linguagens").querySelector(".nivel")
+        barra.style.width = barra.dataset.progresso + "%"
+
+
+        if (porcentagem >= 75) {
+            barra.style.background = "#22d3ee";
+            nivel.textContent = "Avançado";
+            nivel.style.color = "#22d3ee";
+
+        } else if (porcentagem >= 50) {
+            barra.style.background = "#59d664";
+            nivel.textContent = "Intermediário";
+            nivel.style.color = "#59d664";
+
+        } else {
+            barra.style.background = "#dc70f4";
+            nivel.textContent = "Iniciante";
+            nivel.style.color = "#dc70f4";
+        }
+        
+        const por = barra.closest(".linguagens").querySelector(".por")
+        por.textContent = `${porcentagem}% / 100%`
+        
+    })    
+})
+
+
+   
